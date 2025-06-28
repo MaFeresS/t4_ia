098acd3f4447a5dfcc3ad90cb62c5383fd786c97
MaFeres <maferes@miuandes.cl>
2025-06-27 23:01:30 -0400

    I did something

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ import torch
 import torch.nn as nn
 
 CLASSIFIER=['mlp','svm','rf']
+DATAFILE=['clip','dinov2','resnet34']
 
-for m in CLASSIFIER:
-    if m=='mlp':
-        model = MLPClassifier((256,),'relu',random_state=10)
-    elif m=='svm':
-        model = SVC(kernel='rbf')
-    elif m=='rf':
-        model =RandomForestClassifier(random_state=10)
+for d in DATAFILE:
+
+    file=np.load(f"feat_{d}_VocPascal.npy")
+    with open("VocPascal/val_voc.txt","r") as labelfile:
+        labels=[]
+        for i in labelfile.readlines():
+            labels.append(i.split("\t")[1])
+    labels=np.array(labels)
+
+    for m in CLASSIFIER:
+        if m=='mlp':
+            model = MLPClassifier((256,),'relu',random_state=10)
+        elif m=='svm':
+            model = SVC(kernel='rbf')
+        elif m=='rf':
+            model =RandomForestClassifier(random_state=10)
+        mod=model.fit(file)
+        labels_=mod.predict()
